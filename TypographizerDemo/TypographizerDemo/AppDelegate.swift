a994de1b36f4b6f55b59e4a812f065125f8473db
Frank Rausch <git@frankrausch.com>
2017-03-16 17:38:06 +0100

    Add macOS demo project.

---
+++ TypographizerDemo/TypographizerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TypographizerDemo
+//
+//  Created by Frank Rausch on 2017-03-15.
+//  Copyright © 2017 Frank Rausch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TypographizerDemo/TypographizerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TypographizerDemo
+//
+//  Created by Frank Rausch on 2017-03-15.
+//  Copyright © 2017 Frank Rausch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
