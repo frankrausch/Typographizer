de90b2e33f285bcd50e1f6282c178f2020d5188f
Frank Rausch <git@frankrausch.com>
2017-03-17 12:32:12 +0100

    Finish first version of macOS demo app.

--- TypographizerDemo/TypographizerDemo/ViewController.swift
+++ TypographizerDemo/TypographizerDemo/ViewController.swift
@@ -8,20 +8,95 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
+class ViewController: NSViewController, NSTextViewDelegate {
+    
+    @IBOutlet weak var beforeTextView: NSTextView! {
         didSet {
-        // Update the view, if already loaded.
+            beforeTextView.delegate = self
         }
     }
-
-
+    
+    @IBOutlet weak var afterTextView: NSTextView!
+    
+    @IBOutlet weak var languageCodePopUpButton: NSPopUpButton!
+    
+    @IBOutlet weak var htmlCheckbox: NSButton!
+    @IBOutlet weak var debugCheckbox: NSButton!
+    @IBOutlet weak var measurePerformanceCheckbox: NSButton!
+    
+    let supportedLanguageCodes = [
+        "bs",
+        "cs",
+        "da",
+        "de",
+        "en",
+        "et",
+        "fi",
+        "fr",
+        "he",
+        "hu",
+        "is",
+        "ja",
+        "lt",
+        "lv",
+        "nl",
+        "nn",
+        "no",
+        "pl",
+        "ro",
+        "ru",
+        "sk",
+        "sl",
+        "sv",
+        ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let font = NSFont(name: "Menlo", size: 20)
+        self.beforeTextView.font = font
+        self.afterTextView.font = font
+        
+        self.languageCodePopUpButton.removeAllItems()
+        self.languageCodePopUpButton.addItems(withTitles: self.supportedLanguageCodes)
+        self.languageCodePopUpButton.selectItem(withTitle: "en")
+        
+        self.beforeTextView.string = "\"This 'magic' piece of code fixes dumb quotes and apostrophes.\"\n\n<p class=\"intro\">\n  It doesn't 'break' HTML.\n</p>\n\n<code>\n  let s = \"Hello, world!\"\n</code>"
+        
+        self.refresh()
+    }
+    
+    private func refresh() {
+        var s: String = self.beforeTextView.string!
+        
+        let language = self.languageCodePopUpButton.titleOfSelectedItem ?? "en"
+        let isHTML: Bool = self.htmlCheckbox.state == 1
+        let debug: Bool = self.debugCheckbox.state == 1
+        let measurePerformance: Bool = self.measurePerformanceCheckbox.state == 1
+        
+        // This is where the magic happens:
+        s = s.typographized(language: language, isHTML: isHTML, debug: debug, measurePerformance: measurePerformance)
+        
+        self.afterTextView.string = s
+    }
+    
+    func textDidChange(_ notification: Notification) {
+        self.refresh()
+    }
+    
+    @IBAction func debugCheckboxDidChange(_ sender: Any) {
+        self.refresh()
+    }
+    
+    @IBAction func htmlCheckboxDidChange(_ sender: Any) {
+        self.refresh()
+    }
+    @IBAction func measurePerformanceCheckboxDidChange(_ sender: Any) {
+        self.refresh()
+    }
+    
+    @IBAction func languageCodePopUpButtonDidChange(_ sender: NSPopUpButton) {
+        self.refresh()
+    }
 }
 
