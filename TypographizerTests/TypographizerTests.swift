d558d8cabf8c1306d05dca9b5e7caf8d36fd109a
Frank Rausch <git@frankrausch.com>
2017-05-19 17:23:10 +0200

    Add HTML to unit test cases. Always test apostrophes when testing quotes.

--- TypographizerTests/TypographizerTests.swift
+++ TypographizerTests/TypographizerTests.swift
@@ -12,67 +12,67 @@ import XCTest
 
 class TypographizerTests: XCTestCase {
 
-    let string = "The 'quick' brown fox jumps over the \"lazy\" dog."
+    let string = "The 'quick' brown fox jumps over the \"lazy\" dog's ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
 
     func testTypographized_he() {
-        let expected = "The \'quick\' brown fox jumps over the \"lazy\" dog."
-        XCTAssertEqual(self.string.typographized(language: "he"), expected)
+        let expected = "The \'quick\' brown fox jumps over the \"lazy\" dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "he", isHTML: true), expected)
     }
 
     func testTypographized_cs_da_de_et_is_lt_lv_sk_sl() {
-        let expected = "The ‚quick‘ brown fox jumps over the „lazy“ dog."
-        XCTAssertEqual(self.string.typographized(language: "cs"), expected)
-        XCTAssertEqual(self.string.typographized(language: "da"), expected)
-        XCTAssertEqual(self.string.typographized(language: "de"), expected)
-        XCTAssertEqual(self.string.typographized(language: "et"), expected)
-        XCTAssertEqual(self.string.typographized(language: "is"), expected)
-        XCTAssertEqual(self.string.typographized(language: "lt"), expected)
-        XCTAssertEqual(self.string.typographized(language: "lv"), expected)
-        XCTAssertEqual(self.string.typographized(language: "sk"), expected)
-        XCTAssertEqual(self.string.typographized(language: "sl"), expected)
+        let expected = "The ‚quick‘ brown fox jumps over the „lazy“ dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "cs", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "da", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "de", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "et", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "is", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "lt", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "lv", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "sk", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "sl", isHTML: true), expected)
     }
 
     func testTypographized_de_CH() {
-        let expected = "The ‹quick› brown fox jumps over the «lazy» dog."
-        XCTAssertEqual(self.string.typographized(language: "de_CH"), expected)
+        let expected = "The ‹quick› brown fox jumps over the «lazy» dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "de_CH", isHTML: true), expected)
     }
 
     func testTypographized_bs_fi_sv() {
-        let expected = "The ’quick’ brown fox jumps over the ”lazy” dog."
-        XCTAssertEqual(self.string.typographized(language: "bs"), expected)
-        XCTAssertEqual(self.string.typographized(language: "fi"), expected)
-        XCTAssertEqual(self.string.typographized(language: "sv"), expected)
+        let expected = "The ’quick’ brown fox jumps over the ”lazy” dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "bs", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "fi", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "sv", isHTML: true), expected)
     }
 
     func testTypographized_fr() {
-        let expected = "The ‹ quick › brown fox jumps over the « lazy » dog."
-        XCTAssertEqual(self.string.typographized(language: "fr"), expected)
+        let expected = "The ‹ quick › brown fox jumps over the « lazy » dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "fr", isHTML: true), expected)
     }
 
     func testTypographized_hu_pl_ro() {
-        let expected = "The ’quick’ brown fox jumps over the „lazy” dog."
-        XCTAssertEqual(self.string.typographized(language: "hu"), expected)
-        XCTAssertEqual(self.string.typographized(language: "pl"), expected)
-        XCTAssertEqual(self.string.typographized(language: "ro"), expected)
+        let expected = "The ’quick’ brown fox jumps over the „lazy” dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "hu", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "pl", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "ro", isHTML: true), expected)
     }
 
     func testTypographized_ja() {
-        let expected = "The 『quick』 brown fox jumps over the 「lazy」 dog."
-        XCTAssertEqual(self.string.typographized(language: "ja"), expected)
+        let expected = "The 『quick』 brown fox jumps over the 「lazy」 dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "ja", isHTML: true), expected)
     }
 
     func testTypographized_ru_no_nn() {
-        let expected = "The ’quick’ brown fox jumps over the «lazy» dog."
+        let expected = "The ’quick’ brown fox jumps over the «lazy» dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
 
-        XCTAssertEqual(self.string.typographized(language: "ru"), expected)
-        XCTAssertEqual(self.string.typographized(language: "no"), expected)
-        XCTAssertEqual(self.string.typographized(language: "nn"), expected)
+        XCTAssertEqual(self.string.typographized(language: "ru", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "no", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "nn", isHTML: true), expected)
     }
 
     func testTypographized_en_nl() {
-        let expected = "The ‘quick’ brown fox jumps over the “lazy” dog."
-        XCTAssertEqual(self.string.typographized(language: "en"), expected)
-        XCTAssertEqual(self.string.typographized(language: "nl"), expected)
+        let expected = "The ‘quick’ brown fox jumps over the “lazy” dog’s ear. This is <code style=\"font: 'SF UI'\">\"HTML\"</code>."
+        XCTAssertEqual(self.string.typographized(language: "en", isHTML: true), expected)
+        XCTAssertEqual(self.string.typographized(language: "nl", isHTML: true), expected)
     }
 
     func testApostrophe() {
