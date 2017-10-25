8f8916ed7cd5e425f407a49d683a24c90d4f2329
Frank Rausch <git@frankrausch.com>
2017-10-25 17:27:39 +0200

    Fix targets for iOS, watchOS, and tvOS frameworks.

---
+++ Typographizer-iOS/Typographizer.h
@@ -0,0 +1,19 @@
+//
+//  Typographizer.h
+//  Typographizer
+//
+//  Created by Frank Rausch on 2017-10-25.
+//  Copyright © 2017 Frank Rausch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Typographizer.
+FOUNDATION_EXPORT double TypographizerVersionNumber;
+
+//! Project version string for Typographizer.
+FOUNDATION_EXPORT const unsigned char TypographizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Typographizer_iOS/PublicHeader.h>
+
+
